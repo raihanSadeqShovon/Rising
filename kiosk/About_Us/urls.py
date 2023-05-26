4e26d208f25f909bbc7a5010e01390e7c6a400eb
raihan-sadeq <raihanshovon@yahoo.com>
2023-05-26 12:33:25 +0600

    Show database table data to user

--- kiosk/About_Us/urls.py
+++ kiosk/About_Us/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('a/', views.about_u)
+    path('a/', views.about_u),
+    path('t/' , views.teachers_info),
 ]
--- kiosk/About_Us/views.py
+++ kiosk/About_Us/views.py
@@ -1,6 +1,11 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from About_Us.models import Teacher
 
 # Create your views here.
 def about_u(Request):
     return render(Request, 'about/about.html')
+
+def teachers_info(Request):
+    teach = Teacher.objects.all()
+    return render(Request, 'about/t.html', {'tch' : teach})
