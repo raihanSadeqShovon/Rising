125da7a71bf830761a6ac570cbb3215c8beaaaca
raihan-sadeq <raihanshovon@yahoo.com>
2023-06-10 11:38:35 +0600

    Creating django form and ordering field

--- kiosk/Blogs/urls.py
+++ kiosk/Blogs/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('b/', views.blogs1)
+    path('b/', views.blogs1),
+    path('f/', views.showformdata),
 ]
--- kiosk/Blogs/views.py
+++ kiosk/Blogs/views.py
@@ -1,7 +1,13 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from . forms import TeachersRegistration
 
 # Create your views here.
 def blogs1(request):
     return render(request, 'blogs/blogs.html')
     
+
+def showformdata(request):
+    fm = TeachersRegistration()
+    fm.order_fields(field_order=['email','first_name','last_name'])
+    return render(request, 'blogs/forms.html', {'form': fm})
